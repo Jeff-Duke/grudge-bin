0c00741fa4e3df24e686af4561fff922d8161694
Jeff Duke <jeff.duke@gmail.com>
2017-01-10 15:53:52 -0700

    refactor code.  add delete functionality.  only delete offender from list if they have no more offenses

--- src/components/Grudge.js
+++ src/components/Grudge.js
@@ -1,18 +1,28 @@
 import React from 'react';
 
-const Grudge = ({grudge, updateForgiven}) => {
-  const { offender, offense, forgiven, key } = grudge;
+const Grudge = ({grudge, updateForgiven, deleteGrudge}) => {
+  const {offender, offense, forgiven, key} = grudge;
   return (
     <article className='grudge' key={key}>
       <h3>{offender}</h3>
       <p>{offense}</p>
       <label htmlFor='forgiven'>Forgiven</label>
       <input
-        type='checkbox' 
-        name='forgiven' 
+        type='checkbox'
+        name='forgiven'
         value={forgiven}
-        onClick={(e) => {updateForgiven(e)}}
-      />
+        onClick={(e) => {
+        updateForgiven(grudge)
+      }}/>
+      <button
+        name='deleteGrudge'
+        aria-label='Delete Grudge Button'
+        children='X'
+        onClick={(e) => {
+          e.preventDefault();
+          deleteGrudge(grudge);
+        }}>
+      </button>
     </article>
   );
 };
