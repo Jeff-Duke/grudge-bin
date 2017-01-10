cf301082d291dbc5b228b206dcf28d36af05da0b
Jeff Duke <jeff.duke@gmail.com>
2017-01-10 10:13:58 -0700

    configure base repo and get rendering on the screen

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from '../../public/images/logo.svg';
+import '../styles/css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <section className="App">
+        
+      </section>
+    );
+  }
+}
+
+export default App;
