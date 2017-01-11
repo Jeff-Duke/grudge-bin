080f1a22a7404ed5c2339bb6a36ffa970c75f7bb
Jeff Duke <jeff.duke@gmail.com>
2017-01-10 17:46:39 -0700

    fix typo

--- src/components/OffenderList.js
+++ src/components/OffenderList.js
@@ -5,7 +5,7 @@ const OffenderList = ({ offenders, updateOffender, grudgesToShow }) => {
   return (
     <section>
       <h2> Those dastardly dogs what wronged you: </h2>
-      <h4> Select a curr from the list to see what wongdoings they've done to ye</h4>
+      <h4> Select a curr from the list to see what wongdoings they have done to ye</h4>
           <ul>
             {offenders && 
               offenders.map((offender, index) => 
